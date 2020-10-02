50896cef1ed6ef1dea2d1686f8cd09f82c3bc5dd
Patrik Sundberg <patrik.sundberg@hey.com>
2020-10-02 16:58:18 +0100

    Use normal escape - relay on lctrl tap for ` and ~.

--- keyboards/womier/k66/keymaps/sundbp/keymap.c
+++ keyboards/womier/k66/keymaps/sundbp/keymap.c
@@ -9,7 +9,7 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
-    KC_GESC          , KC_1       , KC_2     , KC_3    , KC_4    , KC_5    , KC_6       , KC_7      , KC_8    , KC_9      , KC_0    , KC_MINS , KC_EQL  , KC_BSPC , KC_PGUP ,
+    KC_ESC           , KC_1       , KC_2     , KC_3    , KC_4    , KC_5    , KC_6       , KC_7      , KC_8    , KC_9      , KC_0    , KC_MINS , KC_EQL  , KC_BSPC , KC_PGUP ,
     KC_TAB           , KC_Q       , KC_W     , KC_E    , KC_R    , KC_T    , KC_Y       , KC_U      , KC_I    , KC_O      , KC_P    , KC_LBRC , KC_RBRC , KC_BSLS , KC_PGDN ,
     LCTL_T(KC_GRAVE) , KC_A       , KC_S     , KC_D    , KC_F    , KC_G    , KC_H       , KC_J      , KC_K    , KC_L      , KC_SCLN , KC_QUOT , KC_ENT            ,
     KC_LSPO          , KC_Z       , KC_X     , KC_C    , KC_V    , KC_B    , KC_N       , KC_M      , KC_COMM , KC_DOT    , KC_SLSH , KC_RSPC           , KC_UP   ,
@@ -40,19 +40,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______          , _______    , _______  ,                     _______                        , _______ , _______   , _______ ,           RGB_HUD , RGB_SAD , RGB_HUI
     )
 };
-
-uint32_t layer_state_set_user(uint32_t state) {
-    /*
-      switch (biton32(state)) {
-      case _QWERTY:
-      // some action relating to layer change!
-      break;
-      default:
-      break;
-      }
-     */
-    return state;
-}
 
 void rgb_matrix_indicators_user(void) {
     switch (biton32(layer_state)) {
